3f7d784e38e846e295e384aed5f033d4f2285821
Ariel-Fumagalli <arielfumagalli@gmail.com>
2021-07-08 18:29:31 -0300

    implementación de Firebase en ItemListContainer e ItemDetailContainer

---
+++ src/firebase/index.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore';
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyDkeJUlYkmVkT6U3lC9WpgdnICM7lSFYKo",
+    authDomain: "rohe-store.firebaseapp.com",
+    //databaseURL: "https://rohe-store.firebaseio.com",
+    projectId: "rohe-store",
+    storageBucket: "rohe-store.appspot.com",
+    messagingSenderId: "385247012160",
+    appId: "1:385247012160:web:af4192be878038e37e46a5"
+  });
+
+export function getFirebase(){
+	return app;
+}
+
+export function getFirestore() {
+	return firebase.firestore(app);
+}
